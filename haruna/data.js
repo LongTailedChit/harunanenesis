6c49789eed28d545993ec4672a07c36d2470b6a0
LongTailedChit <snivybeliever@gmail.com>
2026-04-08 21:33:58 +0900

    Add files via upload

---
+++ haruna/data.js
@@ -0,0 +1,10 @@
+﻿let songs = [];
+
+async function loadSongsFromFile() {
+    try {
+        const res = await fetch('data/songs.json');
+        songs = await res.json();
+    } catch (e) {
+        console.error('楽曲データ読み込み失敗', e);
+    }
+}
