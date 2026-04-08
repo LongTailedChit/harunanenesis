00bdead5d02efbc00a23533d01b2c0880eeb7c33
LongTailedChit <snivybeliever@gmail.com>
2026-04-08 22:45:26 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 
 async function loadSongsFromFile() {
     try {
-        const res = await fetch('songs.json');
+        const res = await fetch('data/songs.json');
         songs = await res.json();
     } catch (e) {
         console.error('楽曲データ読み込み失敗', e);
