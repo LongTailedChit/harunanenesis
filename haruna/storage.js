efb192cefb1746d8754960d37772213bf1abf28e
LongTailedChit <snivybeliever@gmail.com>
2026-04-08 21:41:55 +0900

    Delete haruna directory

--- haruna/storage.js
+++
@@ -1,7 +0,0 @@
-﻿function loadHistory() {
-    return JSON.parse(localStorage.getItem('history') || '[]');
-}
-
-function saveHistory(data) {
-    localStorage.setItem('history', JSON.stringify(data));
-}
